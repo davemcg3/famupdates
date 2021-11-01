f99b3af3a8b66409162b27ec6c78bbf5ff2f54be
davemcg3 <dave.mcgrath3@gmail.com>
2021-11-01 16:50:48 -0600

    Trying to find the right combination of dependencies

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,25 +1,25 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const environment = import('./environment')
+const environment = require('./environment')
 
-const customConfig = {
-  resolve: {
-    fallback: {
-      dgram: false,
-      fs: false,
-      net: false,
-      tls: false,
-      child_process: false
-    }
-  }
-};
-
-environment.config.delete('node.dgram')
-environment.config.delete('node.fs')
-environment.config.delete('node.net')
-environment.config.delete('node.tls')
-environment.config.delete('node.child_process')
-
-environment.config.merge(customConfig);
+// const customConfig = {
+//   resolve: {
+//     fallback: {
+//       dgram: false,
+//       fs: false,
+//       net: false,
+//       tls: false,
+//       child_process: false
+//     }
+//   }
+// };
+//
+// environment.config.delete('node.dgram')
+// environment.config.delete('node.fs')
+// environment.config.delete('node.net')
+// environment.config.delete('node.tls')
+// environment.config.delete('node.child_process')
+//
+// environment.config.merge(customConfig);
 
 module.exports = environment.toWebpackConfig()
